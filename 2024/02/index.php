895f8f2453e0f06acbcc11025b22201126964f1c
Wladimir Börner <wladimir.boerner@uni-oldenburg.de>
2024-12-04 22:58:25 +0100

    2024 02 end

--- 2024/02/index.php
+++ 2024/02/index.php
@@ -16,33 +16,39 @@ function part01()
     foreach ($input as $report) {
         $levels = explode(' ', $report);
         $originLevel = $levels;
-        $tryLevels = [$levels];
+        $tryLevels = [$levels];#
+        $firstError = true;
         while ($levels = array_pop($tryLevels)) {
             $previousLevel = array_shift($levels);
             $previousDiff = 0;
-            $retry = true;
             foreach ($levels as $key => $level) {
                 $diff = (int)$previousLevel - (int)$level;
                 $increase = $diff * $previousDiff;
                 $previousDiff = $diff;
                 $absDiff = abs($diff);
                 $previousLevel = $level;
-                if ($absDiff < 1 || $absDiff > 3 || $increase < 0 && !$re  try) {
-                    $firstPossibility = $originLevel;
-                    $secondPossibility = $originLevel;
-                    unset($firstPossibility[$key]);
-                    unset($secondPossibility[$key + 1]);
-                    reset($firstPossibility);
-                    reset($secondPossibility);
-                    $tryLevels[] = $firstPossibility;
-                    $tryLevels[] = $secondPossibility;
-                    $retry = true;
-                    continue 2;
-                } elseif ($retry) {
+                if ($absDiff < 1 || $absDiff > 3 || $increase < 0) {
+                    if ($firstError) {
+                        $firstPossibility = $originLevel;
+                        $secondPossibility = $originLevel;
+                        $thirdPossibility = $originLevel;
+                        unset($thirdPossibility[$key - 1]);
+                        unset($firstPossibility[$key]);
+                        unset($secondPossibility[$key + 1]);
+                        reset($firstPossibility);
+                        reset($secondPossibility);
+                        $tryLevels[] = $firstPossibility;
+                        $tryLevels[] = $secondPossibility;
+                        $tryLevels[] = $thirdPossibility;
+                        $firstError = false;
+                        continue 2;
+                    } elseif (count($tryLevels) != 0) {
+                        continue 2;
+                    }
                     continue 3;
                 }
             }
-
+            breaK;
         }
         $safeResult++;
     }
