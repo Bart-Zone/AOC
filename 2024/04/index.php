4079ac20f58f592e345b2143886433b1f5fe09a4
Wladimir Börner <wladimir.boerner@uni-oldenburg.de>
2024-12-06 18:49:53 +0100

    2024 04

--- 2024/04/index.php
+++ 2024/04/index.php
@@ -12,21 +12,120 @@ function part01()
 {
     $input = getFileContent('input');
     $example = getFileContent('example');
-    $endResult = 0;
+    $xmasFound = 0;
+    $matrix = [];
     foreach ($input as $line) {
-
+        $matrix[] = str_split($line);
     }
-    echo $endResult;
+    $xLength = count($matrix[0]);
+    $yLength = count($matrix);
+    for ($y = 0; $y < $yLength; $y++) {
+        for ($x = 0; $x < $xLength; $x++) {
+            if ($matrix[$y][$x] === 'X') {
+                //check for diagonal left up
+                if (isset($matrix[$y - 3][$x - 3]) && $matrix[$y - 3][$x - 3] === 'S') {
+                    if ($matrix[$y - 1][$x - 1] === 'M' && $matrix[$y - 2][$x - 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check for up
+                if (isset($matrix[$y - 3][$x]) && $matrix[$y - 3][$x] === 'S') {
+                    if ($matrix[$y - 1][$x] === 'M' && $matrix[$y - 2][$x] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check diagonal right up
+                if (isset($matrix[$y - 3][$x + 3]) && $matrix[$y - 3][$x + 3] === 'S') {
+                    if ($matrix[$y - 1][$x + 1] === 'M' && $matrix[$y - 2][$x + 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check for right
+                if (isset($matrix[$y][$x + 3]) && $matrix[$y][$x + 3] === 'S') {
+                    if ($matrix[$y][$x + 1] === 'M' && $matrix[$y][$x + 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check for diagonal right down
+                if (isset($matrix[$y + 3][$x + 3]) && $matrix[$y + 3][$x + 3] === 'S') {
+                    if ($matrix[$y + 1][$x + 1] === 'M' && $matrix[$y + 2][$x + 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check for down
+                if (isset($matrix[$y + 3][$x]) && $matrix[$y + 3][$x] === 'S') {
+                    if ($matrix[$y + 1][$x] === 'M' && $matrix[$y + 2][$x] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //check for diagonal left down
+                if (isset($matrix[$y + 3][$x - 3]) && $matrix[$y + 3][$x - 3] === 'S') {
+                    if ($matrix[$y + 1][$x - 1] === 'M' && $matrix[$y + 2][$x - 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+                //chech for left
+                if (isset($matrix[$y][$x - 3]) && $matrix[$y][$x - 3] === 'S') {
+                    if ($matrix[$y][$x - 1] === 'M' && $matrix[$y][$x - 2] === 'A') {
+                        $xmasFound++;
+                    }
+                }
+            }
+        }
+    }
+
+    echo $xmasFound;
 }
 
 function part02()
 {
     $input = getFileContent('input');
     $example = getFileContent('example');
-    $endResult = 0;
+    $xMasFound = 0;
+    $matrix = [];
     foreach ($input as $line) {
+        $matrix[] = str_split($line);
+    }
+    $xLength = count($matrix[0]);
+    $yLength = count($matrix);
+    for ($y = 0; $y < $yLength; $y++) {
+        for ($x = 0; $x < $xLength; $x++) {
+            if ($matrix[$y][$x] === 'A') {
+                //check for diagonal left up
+                if (isset($matrix[$y - 1][$x - 1]) && $matrix[$y - 1][$x - 1] === 'M'
+                    && isset($matrix[$y + 1][$x - 1]) && $matrix[$y + 1][$x - 1] === 'M'
+                    && isset($matrix[$y - 1][$x + 1]) && $matrix[$y - 1][$x + 1] === 'S'
+                    && isset($matrix[$y + 1][$x + 1]) && $matrix[$y + 1][$x + 1] === 'S'
+                ) {
+                    $xMasFound++;
+                }
 
+                if (isset($matrix[$y - 1][$x - 1]) && $matrix[$y - 1][$x - 1] === 'S'
+                    && isset($matrix[$y + 1][$x - 1]) && $matrix[$y + 1][$x - 1] === 'S'
+                    && isset($matrix[$y - 1][$x + 1]) && $matrix[$y - 1][$x + 1] === 'M'
+                    && isset($matrix[$y + 1][$x + 1]) && $matrix[$y + 1][$x + 1] === 'M'
+                ) {
+                    $xMasFound++;
+                }
+
+                if (isset($matrix[$y - 1][$x - 1]) && $matrix[$y - 1][$x - 1] === 'M'
+                    && isset($matrix[$y + 1][$x - 1]) && $matrix[$y + 1][$x - 1] === 'S'
+                    && isset($matrix[$y - 1][$x + 1]) && $matrix[$y - 1][$x + 1] === 'M'
+                    && isset($matrix[$y + 1][$x + 1]) && $matrix[$y + 1][$x + 1] === 'S'
+                ) {
+                    $xMasFound++;
+                }
+
+                if (isset($matrix[$y - 1][$x - 1]) && $matrix[$y - 1][$x - 1] === 'S'
+                    && isset($matrix[$y + 1][$x - 1]) && $matrix[$y + 1][$x - 1] === 'M'
+                    && isset($matrix[$y - 1][$x + 1]) && $matrix[$y - 1][$x + 1] === 'S'
+                    && isset($matrix[$y + 1][$x + 1]) && $matrix[$y + 1][$x + 1] === 'M'
+                ) {
+                    $xMasFound++;
+                }
+            }
+        }
     }
 
-    echo $endResult;
+    echo $xMasFound;
 }
